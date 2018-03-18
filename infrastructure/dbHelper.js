6f2205016a5e2d766d979710d2d71288816429b9
Maxim Akulin <maxim.akulin@gmail.com>
2018-03-18 15:26:02 +0500

    mock db helper

---
+++ infrastructure/dbHelper.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class DBHelper {
+    static get(query) {
+        throw new Error();        
+    }
+
+    static add(item) {
+        throw new Error();        
+    }
+
+    static update() {
+        throw new Error();        
+    }
+
+    static remove() {
+        throw new Error();
+    }
+}
