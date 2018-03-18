be030cafa75fcb1712d83a55c814c1d6de8a8482
Maxim Akulin <maxim.akulin@gmail.com>
2018-03-18 18:08:19 +0500

    implemented get and add

--- infrastructure/dbHelper.js
+++ infrastructure/dbHelper.js
@@ -1,12 +1,22 @@
 'use strict';
 
-class DBHelper {
-    static get(query) {
-        throw new Error();        
+const fetch = require('node-fetch');
+const { token, url } = require('../HruDB-config.json');
+
+module.exports = class DBHelper {
+    static get(key, params) {
+        const headers = {'Authorization': token, 'Content-Type': 'plain/text'};
+        return fetch(url + `${key}/all`, { headers: headers })
+            .then(response => response.json());
     }
 
-    static add(item) {
-        throw new Error();
+    static add(key, value) {
+        const headers = {'Authorization': token, 'Content-Type': 'application/json'};
+        fetch(url + key, {
+            method: 'POST',
+            body: JSON.stringify(value),
+            headers: headers
+        });
     }
 
     static update() {
